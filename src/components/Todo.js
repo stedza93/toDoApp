7fc93a6ea88a30676124987b7337cfda54178262
Stefan <stefan@Stefans-Mac-mini.local>
2018-02-09 14:51:56 +0100

    petak komit

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React, { Proptypes } from "react";
+
+const Todo = ({ onclick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? "line-through" : "none"
+    }}
+  >
+    {text}
+  </li>
+);
+Todo.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired
+};
+export default Todo;
